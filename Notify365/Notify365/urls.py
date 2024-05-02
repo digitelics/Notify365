034e13b55d99f4fa2ec67a86f871d783ca4e40ba
digitelics <ruben.rodriguez@digitelics.com>
2024-05-02 14:22:31 -0400

    Subiendo dashboard, login y registration

--- Notify365/Notify365/urls.py
+++ Notify365/Notify365/urls.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import client_view
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('client/', client_view.client, name="client"),
+    path('client/', views.client_view.client, name="client"),
+    path('', views.index, name="dashboard"),
+    path('test/', views.test, name='test'),
+    path('login/', views.login_view.login, name="login"),
+    path('registration/', views.registration_view.registration, name="registration"),
 ]
--- Notify365/Notify365/views.py
+++ Notify365/Notify365/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from customer.views import client_view
+from customers.views import client_view
+from security.views import login_view, registration_view
+
+def index (request):
+     return render(request, 'index.html', {})
+
+def test (request):
+     return render(request, 'test.html', {})
 
