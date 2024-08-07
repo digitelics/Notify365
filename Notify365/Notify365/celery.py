5d9264714c790fb0f8704f4463a10012f5c9bef6
digitelics <ruben.rodriguez@digitelics.com>
2024-08-06 23:00:33 -0400

    Ajustando configuracion de celery

--- Notify365/Notify365/celery.py
+++ Notify365/Notify365/celery.py
@@ -2,6 +2,8 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Notify365.settings')
 
@@ -11,5 +13,5 @@ app = Celery('Notify365')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Cargar automáticamente los módulos de tareas de todas las aplicaciones registradas en Django
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
