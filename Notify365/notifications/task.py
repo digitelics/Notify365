1241d3a46703274ac995cb4cc28e3db5730207c6
digitelics <ruben.rodriguez@digitelics.com>
2024-08-07 00:13:12 -0400

    Corrigiendo importacion del metodo para enviar los mensajes de texto en las tareas atumaticas

--- Notify365/notifications/task.py
+++ Notify365/notifications/task.py
@@ -8,10 +8,7 @@ from security.models import CustomUser as User, Suscription
 from utils import template_text
 from django.db.models import Q, Exists, OuterRef
 from datetime import timedelta
-from utils import send_text_notification
-
-
-
+from utils.send_text_notification import send_sms
 import logging
 
 
@@ -40,7 +37,7 @@ def send_birthday_notifications():
                     date=timezone.now(),
                     channel=template.channel_to,
                 )
-                send_text_notification(customer.phone, text)
+                send_sms(customer.phone, text)
                 total_notifications_sent += 1
         else:
             logger.info(f"No template found for subscription {suscription.id}")
@@ -68,7 +65,7 @@ def send_document_notifications():
                     date=timezone.now(),
                     channel=template.channel_to,
                 )
-                send_text_notification(customer.phone, text)
+                send_sms(customer.phone, text)
                 total_notifications_sent += 1
         else:
             logger.info(f"No template found for subscription {suscription.id}")
@@ -98,7 +95,7 @@ def send_expiry_notifications():
                     date=timezone.now(),
                     channel=template.channel_to,
                 )
-                send_text_notification(customer.phone, text)
+                send_sms(customer.phone, text)
                 total_notifications_sent += 1
         else:
             logger.info(f"No template found for subscription {suscription.id}")
@@ -129,7 +126,7 @@ def send_next_expiry_notifications():
                     date=timezone.now(),
                     channel=template.channel_to,
                 )
-                send_text_notification(customer.phone, text)
+                send_sms(customer.phone, text)
                 total_notifications_sent += 1
         else:
             logger.info(f"No template found for subscription {suscription.id}")
@@ -160,7 +157,7 @@ def send_expiry_tomorrow_notifications():
                     date=timezone.now(),
                     channel=template.channel_to,
                 )
-                send_text_notification(customer.phone, text)
+                send_sms(customer.phone, text)
                 total_notifications_sent += 1
         else:
             logger.info(f"No template found for subscription {suscription.id}")
